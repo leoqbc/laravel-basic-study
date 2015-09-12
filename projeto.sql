ee1ef2788289723a13007f7c62e7615765f11788
Leonardo Tumadjian <tumadjian@gmail.com>
2015-09-12 03:07:58 -0300

    Altera SQL para correto

--- projeto.sql
+++ projeto.sql
@@ -1,8 +1,9 @@
 create table todos
 (
-    id int primary key auto_increment,
-    titulo varchar(60) not null,
-    descricao varchar(255) not null,
-    prioridade enum('1', '2', '3') not null,
-    previsao datetime not null
+	id int primary key auto_increment,
+	titulo varchar(60) not null,
+	descricao varchar(255) not null,
+	prioridade enum('1', '2', '3') not null,
+	previsao datetime not null,
+	concluido tinyint default 0
 );
