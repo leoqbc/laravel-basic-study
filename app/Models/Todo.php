51f99fec2889b5640ba124bd340b60bc9d5a2ae3
Leonardo Tumadjian <tumadjian@gmail.com>
2015-09-14 14:05:02 -0300

    Corrige bug de data e acerta validacao

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Todo extends Model
 {
@@ -12,24 +13,36 @@ class Todo extends Model
     
     public function valid()
     {
-        if ($this->validPrioridade()->fails()) {
-            $this->erro = 'Prioridade inválida';
+        $fields = $this->checkFields();
+        if (!$fields->valid()) {
+            foreach($fields->errors()->all() as $erro) { 
+                $this->erro .= $erro . '<br>';
+            }
             return false;
         }
         return true;
     }
     
-    public function validPrioridade()
+    public function checkFields()
     {
-        return \Validator::make(
-            ['prioridade' => $this->prioridade], 
-            ['prioridade' => 'in:1,2,3']
+        return \Validator::make([   
+                'prioridade' => $this->prioridade, 
+                'previsao' => $this->previsao
+            ],[
+                'prioridade' => 'in:1,2,3',
+                'previsao' => 'date_format:d/m/Y H:i:s'
+            ]
         );
     }
     
     public function setPrevisaoAttribute($data)
     {
-        $this->attributes['previsao'] = date('Y-m-d H:i:s', strtotime($data));
+//        $split_date = explode(' ', $data);
+//        $date = vsprintf('%3$04d-%2$02d-%1$02d ', explode('/', $split_date[0]));
+        $date = Carbon::createFromFormat('d/m/Y H:i:s', $data)
+                    ->toDateTimeString();
+        
+        $this->attributes['previsao'] = $date;
     }
     
     public function getPrevisaoAttribute($data)
